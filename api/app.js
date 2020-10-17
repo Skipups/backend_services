642c7cf58c61d31f456529c00601cacda737a94d
Skipups <svetlana.rovinsky@gmail.com>
2020-10-17 14:06:10 -0400

    needs error handeling

--- api/app.js
+++ api/app.js
@@ -1,8 +1,24 @@
-const app= Express()
+const fs= require('fs')
+const express= require('express')
 
-app.get('/api/reports/:REPORT_ID', function (requestObj, responseObj){
+
+//minimalistic HTTP server framework. Creating an instance of express app. This app object allows me to register routes on it 
+// that mimic HTTP methods. They 2 arguments- the route and route handler function. route handler function is a cb function taking 2 arguments
+// request and response
+const app= express()  
+
+app.get('/api/reports/:report_id', function (request, response){
+  const reportID= request.params.report_id
   //get File
   //send File
+
+
+  //__dirname for the current directory name, 
+  //process for information about the Node process running on the computer and it’s environment
+const path= `${__dirname}/../database/processed/${reportID}.json`;
+console.log('path', path)
+const report = JSON.parse(fs.readFileSync(path, 'utf-8'))
+  response.json(report)
 })
 
 module.exports= app
--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
 const app = require('./app.js')
+
 app.listen(process.env.API_PORT)
-console.log.apply("API listening at http://localhost:" + process.env.API_PORT)
+console.log("API listening at http://localhost:" + process.env.API_PORT)
